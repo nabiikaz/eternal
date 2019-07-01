5e5fcc8df64e24316310f16491e9abde656190c2
nabiikaz <nabiikaz2017@gmail.com>
2019-07-01 04:10:01 +0200

    Update sql.php
    changing db connection string to azures

--- classes/sql.php
+++ classes/sql.php
@@ -9,14 +9,14 @@
  */
  class SQL 
  {
- 	private $db_host = "localhost"	;
- 	private $db_username = "root";
-	private $db_password = "";
+ 	private $db_host = "eternal-db.database.windows.net"	;
+ 	private $db_username = "nabiikaz1995";
+	private $db_password = "Eternal-db";
  	private $db_name 	 = "eternal";
  	public $connection ;
  	function __construct()
  	{
- 		$this->connection = new mysqli($this->db_host, $this->db_username, $this->db_password, $this->db_name);
+ 		$this->connection = new mysqli($this->db_host, $this->db_username, $this->db_password, $this->db_name,1433);
 		if($this->connection->connect_errno > 0){
 		 die('Unable to connect to database [' . $connection->connect_error . ']');
 		}
